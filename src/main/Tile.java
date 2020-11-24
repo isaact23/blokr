25587063bd8cd0f9face21ce991ce9081958c69c
shufflesninja <isaact23@vt.edu>
2020-11-23 22:14:01 -0500

    Begin implementation of flipping/rotating polyominos

--- src/main/Tile.java
+++ src/main/Tile.java
@@ -3,17 +3,102 @@ package main;
 /**
  * A 1 to 5 square piece that can be fitted onto a board.
  */
-public class Tile {
-    private Coordinate[] coordinates;
+public class Tile implements Cloneable {
+
+    private int tileSize;
+
+    // List of coordinates for all orientations of the piece.
+    private Coordinate[][] allCoordinates;
+
+    // Properties defining how a tile can be flipped/rotated to create new tiles.
+    private boolean can_flip;
+    private boolean can_rotate_two; // Two orientations only
+    private boolean can_rotate_four; // All four orientations
     
-    public Tile(Coordinate[] coordinates) {
-        this.coordinates = coordinates;
+    public Tile(Coordinate[] coordinates,
+                boolean can_flip, boolean can_rotate_two, boolean can_rotate_four) {
+        this.tileSize = coordinates.length;
+
+        this.can_flip = can_flip;
+        this.can_rotate_two = can_rotate_two;
+        this.can_rotate_four = can_rotate_four;
+
+        this.generateCoordinates(coordinates);
     }
-    
+
     /**
-     * @return New polyomino equivalent to this one.
+     * Rotate and flip the piece to generate all unique sets of coordinates.
+     * @param coordinates The coordinates for the squares in the tile.
      */
-    public Tile copy() {
-        throw new UnsupportedOperationException("copy() not implemented yet");
+    private void generateCoordinates(Coordinate[] coordinates) {
+        int orientations = 1;
+        if (this.can_flip)
+            orientations = 2;
+        if (this.can_rotate_four)
+            orientations *= 4;
+        else if (this.can_rotate_two)
+            orientations *= 2;
+
+        // Initialize 2D array of coordinates for every orientation
+        this.allCoordinates = new Coordinate[orientations][this.tileSize];
+    }
+
+    /**
+     * Flip the piece over the x axis.
+     * @param coordinates Coordinates to flip.
+     * @return Copied flipped coordinates.
+     */
+    private Coordinate[] flip(Coordinate[] coordinates) {
+        Coordinate boundingCoord = getBoundingCoord(coordinates);
+        Coordinate[] newCoords = new Coordinate[coordinates.length];
+
+        for (int i = 0; i < coordinates.length; i++) {
+            int newX = coordinates[i].x;
+            int newY = boundingCoord.y - coordinates[i].y;
+            newCoords[i] = new Coordinate(newX, newY);
+        }
+
+        return newCoords;
+    }
+
+    /**
+     * Rotate the given coordinates 90 degrees clockwise.
+     * @param coordinates Coordinates to rotate.
+     * @return Copied rotated coordinates.
+     */
+    private Coordinate[] rotate(Coordinate[] coordinates) {
+        Coordinate boundingCoord = getBoundingCoord(coordinates);
+        Coordinate[] newCoords = new Coordinate[coordinates.length];
+
+        for (int i = 0; i < coordinates.length; i++) {
+            int newX = coordinates[i].y;
+        }
+
+        throw new UnsupportedOperationException("rotate() not implemented yet");
+    }
+
+    /**
+     * Get the largest X and Y values in the set of coordinates.
+     * @param coordinates Coordinates to search.
+     * @return Largest X and Y values as a coordinate.
+     */
+    private Coordinate getBoundingCoord(Coordinate[] coordinates) {
+        Coordinate largestCoord = new Coordinate(0, 0);
+        for (int i = 0; i < coordinates.length; i++) {
+            if (coordinates[i].x > largestCoord.x) {
+                largestCoord.x = coordinates[i].x;
+            }
+            if (coordinates[i].y > largestCoord.y) {
+                largestCoord.y = coordinates[i].y;
+            }
+        }
+        return largestCoord;
+    }
+
+    /**
+     * @return New Tile equivalent to this one.
+     */
+    public Tile clone() {
+        throw new UnsupportedOperationException("clone() not implemented yet");
     }
 }
--- src/main/TileData.java
+++ src/main/TileData.java
@@ -25,21 +25,11 @@ public class TileData {
     }
 
     /**
-     * Get a tile by its number.
-     * @param num The number of the tile.
+     * Get list of coordinates for the specified polyomino.
+     * @param num The polyomino number (0 - 20)
      * @return A tile object.
      */
     private static Tile getTileByNumber(int num) {
-        Coordinate[] coordinates = getCoordinatesByNumber(num);
-        return new Tile(coordinates);
-    }
-
-    /**
-     * Get list of coordinates for the specified polyomino.
-     * @param num The polyomino number (0 - 20)
-     * @return List of squares in the polyomino.
-     */
-    private static Coordinate[] getCoordinatesByNumber(int num) {
         if (num < 0 || num > 20) {
             throw new InvalidParameterException("num must be between 0 and 20");
         }
@@ -58,8 +48,11 @@ public class TileData {
             size = 5;
         }
         Coordinate[] coordinates = new Coordinate[size];
+        boolean can_flip = false;
+        boolean can_rotate_two = false;
+        boolean can_rotate_four = false;
 
-        // Get all coordinates in the polyomino.
+        // Get all coordinates and flip/rotation data in the polyomino.
 
         // For most pieces, the square at (0, 0) is occupied.
         coordinates[0] = new Coordinate(0, 0);
@@ -67,27 +60,34 @@ public class TileData {
         switch (num) {
             // 2 squares
             case 1: {
+                can_rotate_two = true;
                 coordinates[1] = new Coordinate(1, 0); break;
             }
             // 3 squares
             case 2: {
+                can_rotate_two = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0); break;
             }
             case 3: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(0, 1); break;
             }
             // 4 squares
             case 4: {
+                can_rotate_two = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(3, 0); break;
             } case 5: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(0, 1); break;
             } case 6: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(1, 1); break;
@@ -96,37 +96,49 @@ public class TileData {
                 coordinates[2] = new Coordinate(0, 1);
                 coordinates[3] = new Coordinate(1, 1); break;
             } case 8: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(1, 1);
                 coordinates[3] = new Coordinate(2, 1); break;
             }
             // 5 squares
             case 9: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(3, 0);
                 coordinates[4] = new Coordinate(0, 1); break;
             } case 10: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(3, 0);
                 coordinates[4] = new Coordinate(1, 1); break;
             } case 11: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(0, 1);
                 coordinates[4] = new Coordinate(1, 1); break;
             } case 12: {
+                can_flip = true;
+                can_rotate_two = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(1, 1);
                 coordinates[3] = new Coordinate(1, 2);
                 coordinates[4] = new Coordinate(2, 2); break;
             } case 13: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(0, 1);
                 coordinates[3] = new Coordinate(0, 2);
                 coordinates[4] = new Coordinate(1, 2); break;
             } case 14: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(1, 1);
                 coordinates[3] = new Coordinate(2, 1);
@@ -138,26 +150,33 @@ public class TileData {
                 coordinates[3] = new Coordinate(0, 1);
                 coordinates[4] = new Coordinate(2, 1); break;
             } case 16: {
+                can_rotate_two = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(3, 0);
                 coordinates[4] = new Coordinate(4, 0); break;
             } case 17: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(1, 1);
                 coordinates[3] = new Coordinate(1, 2);
                 coordinates[4] = new Coordinate(2, 1); break;
             } case 18: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(0, 1);
                 coordinates[4] = new Coordinate(0, 2); break;
             } case 19: {
+                can_flip = true;
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(1, 1);
                 coordinates[3] = new Coordinate(2, 1);
                 coordinates[4] = new Coordinate(3, 1); break;
             } case 20: {
+                can_rotate_four = true;
                 coordinates[1] = new Coordinate(1, 0);
                 coordinates[2] = new Coordinate(2, 0);
                 coordinates[3] = new Coordinate(1, 1);
@@ -165,6 +184,10 @@ public class TileData {
             }
         }
 
-        return coordinates;
+        // Being able to uniquely orient a piece four ways implies two unique orientations.
+        if (can_rotate_four)
+            can_rotate_two = true;
+
+        return new Tile(coordinates, can_flip, can_rotate_two, can_rotate_four);
     }
 }
